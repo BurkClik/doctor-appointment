977039eb11a0befa9b9ba7faaec9442559f8c539
BurkClik <burk.clik@gmail.com>
2021-05-06 22:28:49 +0300

    Add doctor detail informations

--- app/src/main/java/com/example/doctorappointment/data/remote/User.kt
+++ app/src/main/java/com/example/doctorappointment/data/remote/User.kt
@@ -5,6 +5,14 @@ data class User(
     val city: String,
     val gender: String,
     val is_doctor: Boolean,
+    val doctor: Doctor,
     val mail: String,
     val name: String,
-)
+) {
+    data class Doctor(
+        val about: String,
+        val department: String,
+        val hospital: String,
+        val title: String
+    )
+}
